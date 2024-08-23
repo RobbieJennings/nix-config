5169f4288eb3e50268ea61beb8832ea2c0549b48
RobbieJennings <robbie.jennings97@gmail.com>
2024-08-23 20:10:11 +0100

    Enable plymouth and silent boot

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -9,5 +9,20 @@
     boot.loader.grub.enable = lib.mkDefault true;
     boot.loader.grub.efiSupport = lib.mkDefault true;
     boot.loader.grub.efiInstallAsRemovable = lib.mkDefault true;
+
+    # Enable plymouth boot animation
+    boot.initrd.systemd.enable = true;
+    boot.plymouth.enable = true;
+    boot.plymouth.theme = "breeze";
+
+    # Enable "Silent Boot"
+    boot.consoleLogLevel = 0;
+    boot.initrd.verbose = false;
+    boot.kernelParams = [ "quiet" ];
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    boot.loader.timeout = 0;
   };
 }
