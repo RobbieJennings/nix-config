a1b1697439925b8e3f1133056faa8fd580e75e7f
Robbie Jennings <robbie.jennings97@gmail.com>
2024-09-18 16:31:57 +0100

    Update impermenance.nix

--- modules/nixos/core/impermenance.nix
+++ modules/nixos/core/impermenance.nix
@@ -11,13 +11,28 @@
       after = ["dev-mapper-crypted.device"];
       wantedBy = ["initrd.target"];
       script = ''
-        mkdir /btrfs_tmp
-        mount /dev/mapper/crypted /btrfs_tmp
-        if [[ -e /btrfs_tmp/root ]]; then
-          btrfs subvolume delete /btrfs_tmp/root
+        mkdir /mnt
+        mount /dev/mapper/crypted /mnt
+        if [[ -e /mnt/root ]]; then
+          mkdir -p /mnt/old_roots
+          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /mnt/root "/mnt/old_roots/$timestamp"
         fi
-        btrfs subvolume create /btrfs_tmp/root
-        umount /btrfs_tmp
+
+        delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/mnt/$i"
+          done
+          btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /mnt/old_roots/ -maxdepth 1); do
+          delete_subvolume_recursively "$i"
+        done
+
+        btrfs subvolume create /mnt/root
+        umount /mnt
       '';
     };
 
