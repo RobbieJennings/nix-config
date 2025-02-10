888ebabb6f2ad483fba10e9d2285725f2e4fdb5e
robbiejennings <robbie.jennings97@gmail.com>
2025-02-10 18:19:19 +0000

    add check for impermanence flag on platform persist

--- platforms/desktop/persist.nix
+++ platforms/desktop/persist.nix
@@ -1,17 +1,19 @@
 { config, lib, pkgs, inputs, nixosModules, ... }:
 
 {
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      "/etc/nixos"
-      "/root/.ssh"
-      "/root/.config/sops"
-    ];
+  config = lib.mkIf config.impermanence.enable {
+    environment.persistence."/persist" = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/bluetooth"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+        "/etc/nixos"
+        "/root/.ssh"
+        "/root/.config/sops"
+      ];
+    };
   };
 }
--- platforms/server/persist.nix
+++ platforms/server/persist.nix
@@ -1,16 +1,18 @@
 { config, lib, pkgs, inputs, nixosModules, ... }:
 
 {
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      "/var/log"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      "/etc/nixos"
-      "/root/.ssh"
-      "/root/.config/sops"
-    ];
+  config = lib.mkIf config.impermanence.enable {
+    environment.persistence."/persist" = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+        "/etc/nixos"
+        "/root/.ssh"
+        "/root/.config/sops"
+      ];
+    };
   };
 }
