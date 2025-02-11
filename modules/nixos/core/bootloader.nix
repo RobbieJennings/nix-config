59e1a75c28b2517555eaf1bb6dc49915633055e0
robbiejennings <robbie.jennings97@gmail.com>
2025-02-11 03:16:00 +0000

    update conditional logic in bootloader.nix

--- modules/nixos/core/bootloader.nix
+++ modules/nixos/core/bootloader.nix
@@ -6,33 +6,39 @@
     bootloader.pretty = lib.mkEnableOption "enables silent boot with breeze theme";
   };
 
-  config = (lib.mkMerge [
-    (lib.mkIf config.bootloader.enable {
-      # Enable grub bootloader
-      boot.loader.grub.enable = lib.mkDefault true;
-      boot.loader.grub.efiSupport = lib.mkDefault true;
-      boot.loader.grub.efiInstallAsRemovable = lib.mkDefault true;
-    })
+  config = lib.mkIf config.bootloader.enable {
+    boot =
+      if config.bootloader.pretty
+      then {
+        # Enable grub bootloader
+        loader.grub.enable = lib.mkDefault true;
+        loader.grub.efiSupport = lib.mkDefault true;
+        loader.grub.efiInstallAsRemovable = lib.mkDefault true;
 
-    (lib.mkIf config.bootloader.pretty {
-      # Enable breeze grub theme
-      boot.loader.grub.theme = "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
-      boot.loader.grub.splashImage = null;
+        # Enable breeze grub theme
+        loader.grub.theme = "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
+        loader.grub.splashImage = null;
 
-      # Enable plymouth boot animation
-      boot.initrd.systemd.enable = true;
-      boot.plymouth.enable = true;
-      boot.plymouth.theme = "breeze";
+        # Enable plymouth boot animation
+        initrd.systemd.enable = true;
+        plymouth.enable = true;
+        plymouth.theme = "breeze";
 
-      # Enable "Silent Boot"
-      boot.initrd.verbose = false;
-      boot.kernelParams = [ "quiet" ];
+        # Enable "Silent Boot"
+        initrd.verbose = false;
+        kernelParams = [ "quiet" ];
 
-      # Hide the OS choice for bootloaders.
-      # It's still possible to open the bootloader list by pressing any key
-      # It will just not appear on screen unless a key is pressed
-      boot.loader.grub.timeoutStyle = "hidden";
-      boot.loader.timeout = 5;
-    })
-  ]);
+        # Hide the OS choice for bootloaders.
+        # It's still possible to open the bootloader list by pressing any key
+        # It will just not appear on screen unless a key is pressed
+        loader.grub.timeoutStyle = "hidden";
+        loader.timeout = 5;
+      }
+      else {
+        # Enable grub bootloader
+        loader.grub.enable = lib.mkDefault true;
+        loader.grub.efiSupport = lib.mkDefault true;
+        loader.grub.efiInstallAsRemovable = lib.mkDefault true;
+      };
+    };
 }
