fc294a0263e1b005b93bdd970c00eecfe001bfee
Robbie Jennings <robbie.jennings97@gmail.com>
2025-02-16 12:40:13 +0000

    Update default.nix

--- hosts/xps15/default.nix
+++ hosts/xps15/default.nix
@@ -2,7 +2,7 @@
   mkHost = hostname: { inputs, ... }: {
     imports = [
       inputs.disko.nixosModules.disko
-      inputs.nixos-hardware.nixosModules.dell-xps-15-7590
+      inputs.nixos-hardware.nixosModules.dell-xps-15-7590/nvidia
       ./hardware-configuration.nix
       ./disk-configuration.nix
     ];
