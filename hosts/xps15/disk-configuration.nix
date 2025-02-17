2153e8d4305ddccd4c5a8e3e9f8045b7cf0fff41
Robbie Jennings <robbie.jennings97@gmail.com>
2025-02-17 14:54:23 +0000

    Update disk-configuration.nix

--- hosts/xps15/disk-configuration.nix
+++ hosts/xps15/disk-configuration.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "nvme0n1";
+        device = "/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
