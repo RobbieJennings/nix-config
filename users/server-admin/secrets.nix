f51c042567705e57299e5a5f1c05112d7a22235a
robbiejennings <robbie.jennings97@gmail.com>
2025-07-02 01:07:20 +0100

    remove test secret from server admin user

--- users/server-admin/secrets.nix
+++ users/server-admin/secrets.nix
@@ -9,9 +9,7 @@
     sops = {
       defaultSopsFile = ../../secrets/${config.home.username}.yaml;
       age.sshKeyPaths = [ "/home/${config.home.username}/.ssh/id_ed25519" ];
-      secrets = {
-        test_secret = { };
-      };
+      secrets = { };
     };
   };
 }
