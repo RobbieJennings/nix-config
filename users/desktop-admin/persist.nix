10f42ceda116d774dcdf5141a4bf380fcba3f960
robbiejennings <robbie.jennings97@gmail.com>
2025-07-08 15:11:51 +0100

    update user persist directories

--- users/desktop-admin/persist.nix
+++ users/desktop-admin/persist.nix
@@ -16,23 +16,17 @@
       "Games"
       "nix-config"
       {
-        directory = ".gnupg";
-        mode = "0700";
-      }
-      {
         directory = ".ssh";
-        mode = "0700";
-      }
-      {
-        directory = ".nixops";
         mode = "0700";
       }
       {
         directory = ".local/share/keyrings";
         mode = "0700";
       }
-      ".local/share/kwalletd"
-      ".local/share/direnv"
+      {
+        directory = ".local/share/kwalletd";
+        mode = "0700";
+      }
       ".local/share/flatpak"
       ".local/share/Steam"
       ".local/share/PrismLauncher"
--- users/server-admin/persist.nix
+++ users/server-admin/persist.nix
@@ -14,22 +14,13 @@
       "Videos"
       "nix-config"
       {
-        directory = ".gnupg";
-        mode = "0700";
-      }
-      {
         directory = ".ssh";
-        mode = "0700";
-      }
-      {
-        directory = ".nixops";
         mode = "0700";
       }
       {
         directory = ".local/share/keyrings";
         mode = "0700";
       }
-      ".local/share/direnv"
       ".config"
       ".var"
     ];
