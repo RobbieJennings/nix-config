ced6f6e2aa48aaed6e42959b1772052f0775e84c
robbiejennings <robbie.jennings97@gmail.com>
2025-07-18 02:33:13 +0100

    replace grub with systemd-boot

--- modules/nixos/core/bootloader.nix
+++ modules/nixos/core/bootloader.nix
@@ -8,40 +8,29 @@
 
 {
   options = {
-    bootloader.enable = lib.mkEnableOption "grub bootloader";
+    bootloader.enable = lib.mkEnableOption "systemd-boot bootloader";
     bootloader.pretty = lib.mkEnableOption "silent boot with breeze theme";
   };
 
   config = lib.mkMerge [
     (lib.mkIf config.bootloader.enable {
-      boot.loader.grub = {
-        enable = true;
-        efiSupport = true;
-        efiInstallAsRemovable = true;
+      boot.loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
       };
     })
 
     (lib.mkIf (config.bootloader.enable && config.bootloader.pretty) {
       boot = {
-        loader = {
-          grub = {
-            theme = lib.mkDefault "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
-            splashImage = null;
-            timeoutStyle = "hidden";
-          };
-          timeout = lib.mkDefault 5;
-        };
-
+        loader.timeout = lib.mkDefault 0;
         initrd = {
           systemd.enable = true;
           verbose = false;
         };
-
         plymouth = {
           enable = true;
           theme = lib.mkDefault "breeze";
         };
-
         kernelParams = [ "quiet" ];
       };
     })
