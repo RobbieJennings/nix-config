dbdfd7ad4a25f7352020c7542c58cfc4a75a3f99
robbiejennings <robbie.jennings97@gmail.com>
2025-07-23 14:57:21 +0100

    update rclone configuration

--- modules/home-manager/backup/restic.nix
+++ modules/home-manager/backup/restic.nix
@@ -18,15 +18,6 @@
         restic
         restic-browser
       ];
-
-      services.restic.backups.gdrive = {
-        user = "backups";
-        paths = [ "~/Documents" ];
-        timerConfig = {
-          OnCalendar = "00:00";
-          RandomizedDelaySec = "45m";
-        };
-      };
     })
 
     (lib.mkIf (config.backup.restic.enable && config.secrets.enable) {
@@ -36,16 +27,27 @@
         "restic-rclone.conf".content = ''
           [gdrive]
           type = drive
+          scope = drive.file
           client_id = ${config.sops.placeholder."restic/rclone/client_id"}
           client_secret = ${config.sops.placeholder."restic/rclone/client_secret"}
           token = ${config.sops.placeholder."restic/rclone/access_token"}
         '';
       };
 
+      home.activation."rclone.conf" = ''
+        ln -sf ${config.sops.templates."restic-rclone.conf".path} ~/.config/rclone/rclone.conf
+      '';
+
       services.restic.backups.gdrive = {
         repositoryFile = config.sops.templates."restic-repository".path;
         passwordFile = config.sops.templates."restic-password".path;
         rcloneConfigFile = config.sops.templates."restic-rclone.conf".path;
+        user = "backups";
+        paths = [ "~/Documents" ];
+        timerConfig = {
+          OnCalendar = "00:00";
+          RandomizedDelaySec = "45m";
+        };
       };
     })
   ];
