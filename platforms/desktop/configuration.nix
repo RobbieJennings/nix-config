1b011951415223f665005d7baede5c6463a63ec8
robbiejennings <robbie.jennings97@gmail.com>
2025-07-28 00:09:04 +0100

    enable qmk on desktop

--- platforms/desktop/configuration.nix
+++ platforms/desktop/configuration.nix
@@ -19,6 +19,7 @@
 
   desktop.enable = true;
   bootloader.pretty = true;
+  hardware.keyboard.qmk.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
