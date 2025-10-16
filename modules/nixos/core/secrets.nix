693ea68e41d573d8b5017d323f9bc6453bea12d2
robbiejennings <robbie.jennings97@gmail.com>
2025-10-16 22:39:48 +0100

    update secrets module

--- modules/nixos/core/secrets.nix
+++ modules/nixos/core/secrets.nix
@@ -24,7 +24,7 @@
         if config.impermanence.enable then
           lib.mkDefault [ "/persist/root/.ssh/id_ed25519" ]
         else
-          lib.mkDefault [ "/.ssh/id_ed25510" ];
+          lib.mkDefault [ "/root/.ssh/id_ed25510" ];
     };
   };
 }
