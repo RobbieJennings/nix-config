d700b3de9702eb57c2ffdcd392c21a745171ed4e
robbiejennings <robbie.jennings97@gmail.com>
2025-10-27 00:13:57 +0000

    remove unused wallpapers package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 pkgs: {
   vuescan = pkgs.callPackage ./vuescan.nix { };
   epson-v550-plugin = pkgs.callPackage ./epson-v550-plugin.nix { };
-  gruvbox-wallpapers = pkgs.callPackage ./gruvbox-wallpapers.nix { };
 }
