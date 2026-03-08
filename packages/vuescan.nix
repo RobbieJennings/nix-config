a31754216a002d7059cfb2020f468944c9df6e04
robbiejennings <robbie.jennings97@gmail.com>
2026-03-08 01:35:16 +0000

    update vuescan

--- packages/vuescan.nix
+++ packages/vuescan.nix
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://www.hamrick.com/files/vuex6498.tgz";
-    sha256 = "sha256-WdRcB0BDdvztOtEFvzKiNkLyQ/wC3WR25lAKcPuJZh0=";
+    sha256 = "sha256-hIlf6Xh80t5Iy/UmvrHGvUdmrkr7nVlTOaHe8i0dPE0=";
   };
 
   nativeBuildInputs = [
