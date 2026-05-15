4f338ff2d05fd72d9d3d9da4328dcd42f65cddc4
robbiejennings <robbie.jennings97@gmail.com>
2026-05-15 18:36:27 +0100

    update jellyfin

--- modules/server/media/flaresolverr/images.nix
+++ modules/server/media/flaresolverr/images.nix
@@ -15,7 +15,7 @@
       flaresolverrImage = pkgs.dockerTools.pullImage {
         imageName = "flaresolverr/flaresolverr";
         imageDigest = "sha256:7962759d99d7e125e108e0f5e7f3cdbcd36161776d058d1d9b7153b92ef1af9e";
-        sha256 = "sha256-ANmJg+ZQYLXIZrbOSufKR8khSiuJ9S+83DCFCBf2Yf4=";
+        hash = "sha256-ANmJg+ZQYLXIZrbOSufKR8khSiuJ9S+83DCFCBf2Yf4=";
         finalImageTag = "3.4.6";
         arch = "amd64";
       };
--- modules/server/media/jellyfin/images.nix
+++ modules/server/media/jellyfin/images.nix
@@ -14,9 +14,9 @@
     let
       jellyfinImage = pkgs.dockerTools.pullImage {
         imageName = "linuxserver/jellyfin";
-        imageDigest = "sha256:367630b4e4e643c3c1d00bb76f13f3dbe318ad817c01256c358316c7acc3919b";
-        sha256 = "sha256-5Vx7FccwINl85GSwXXc2CX/O+VgIaxsNVuMMDT18itI=";
-        finalImageTag = "10.11.3";
+        imageDigest = "sha256:e622a48adf7193c7d626c2325a40f9b01c932c8e7bf3f50ffa40e7e9b6ce1d39";
+        hash = "sha256-6ywpJewD2TEBbCx9ol9QLzJci2tyKFGO/z9LC0JDJcE=";
+        finalImageTag = "10.11.8";
         arch = "amd64";
       };
     in
