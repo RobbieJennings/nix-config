a8dac30b59b2e2270daad98e1b496672eb8cf908
robbiejennings <robbie.jennings97@gmail.com>
2026-05-15 18:58:39 +0100

    update sonarr

--- modules/server/media/sonarr/images.nix
+++ modules/server/media/sonarr/images.nix
@@ -14,9 +14,9 @@
     let
       sonarrImage = pkgs.dockerTools.pullImage {
         imageName = "linuxserver/sonarr";
-        imageDigest = "sha256:4b8a853b76337cd5de5f69961e23b7d0792ce7bf0a8be083dd7202ef670bfc34";
-        sha256 = "sha256-/wqQhgSMQ8fHwjNS+8n4VtWi/2bREJAPbCaIqeJKMDw=";
-        finalImageTag = "4.0.16";
+        imageDigest = "sha256:60f3b6b5c7647ba2bafd81163acfe34b11117b9b834ebd7fbcc3e5f1b309c7ef";
+        hash = "sha256-ZK7gdBgMWez9O5Ky4Z5pWaoMSPfxBkdjDLb2vM5u5z4=";
+        finalImageTag = "4.0.17";
         arch = "amd64";
       };
     in
