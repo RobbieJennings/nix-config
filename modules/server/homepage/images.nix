575724c41d28cef80b61fed232dee3e9f9b8b26a
robbiejennings <robbie.jennings97@gmail.com>
2026-05-15 21:06:31 +0100

    update homepage

--- modules/server/homepage/images.nix
+++ modules/server/homepage/images.nix
@@ -14,9 +14,9 @@
     let
       homepageImage = pkgs.dockerTools.pullImage {
         imageName = "ghcr.io/gethomepage/homepage";
-        imageDigest = "sha256:0b596092c0b55fe4c65379a428a3fe90bd192f10d1b07d189a34fe5fabe7eedb";
-        sha256 = "sha256-f2vJ6WPGf9Gk4XDjH5tMdvWvHhT2D2HZKWanUJQloRE=";
-        finalImageTag = "v1.10.1";
+        imageDigest = "sha256:d8d784e5090111b6e4c56dfd90e272d2953a2094e87349f647165df0fa6c4401";
+        hash = "sha256-rrtvMdO2AzrzadkjigFqlPrcrtMaByCa5gjGz6Hg3SU=";
+        finalImageTag = "v1.13.1";
         arch = "amd64";
       };
     in
