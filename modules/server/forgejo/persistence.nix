1cc6fb7a0c6c875251438879c8cd18ddac4b9d65
robbiejennings <robbie.jennings97@gmail.com>
2026-05-22 16:26:31 +0100

    update forgejo persistence

--- modules/server/forgejo/persistence.nix
+++ modules/server/forgejo/persistence.nix
@@ -13,9 +13,49 @@
     }:
     {
       config = lib.mkIf config.forgejo.enable {
-        services.k3s.autoDeployCharts.forgejo.values.persistence = {
-          enabled = true;
-          size = "25Gi";
+        system.activationScripts.createForgejoDirs = ''
+          mkdir -p /storage/forgejo
+          chown 1000:1000 /storage/forgejo
+          chmod 775 /storage/forgejo
+        '';
+        services.k3s.autoDeployCharts.forgejo = {
+          values.persistence = {
+            enabled = true;
+            create = false;
+            claimName = "forgejo";
+          };
+          extraDeploy = [
+            {
+              apiVersion = "v1";
+              kind = "PersistentVolume";
+              metadata = {
+                name = "forgejo-pv";
+              };
+              spec = {
+                capacity.storage = "100Gi";
+                accessModes = [ "ReadWriteOnce" ];
+                persistentVolumeReclaimPolicy = "Retain";
+                hostPath = {
+                  path = "/storage/forgejo";
+                  type = "DirectoryOrCreate";
+                };
+              };
+            }
+            {
+              apiVersion = "v1";
+              kind = "PersistentVolumeClaim";
+              metadata = {
+                name = "forgejo";
+                namespace = "forgejo";
+              };
+              spec = {
+                volumeName = "forgejo-pv";
+                resources.requests.storage = "25Gi";
+                accessModes = [ "ReadWriteOnce" ];
+                storageClassName = "";
+              };
+            }
+          ];
         };
       };
     };
