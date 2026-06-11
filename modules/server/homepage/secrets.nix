33282b67d8b7417346a3b068f44ebc1e80f0a75c
robbiejennings <robbie.jennings97@gmail.com>
2026-06-11 18:06:37 +0100

    refactor homepage secrets

--- modules/server/homepage/secrets.nix
+++ modules/server/homepage/secrets.nix
@@ -24,25 +24,60 @@
             };
             stringData = {
               GRAFANA_USERNAME =
-                if config.secrets.grafana.enable then config.sops.placeholder."grafana/username" else "";
+                if (config.grafana.enable && config.secrets.grafana.enable) then
+                  config.sops.placeholder."grafana/username"
+                else
+                  "";
               GRAFANA_PASSWORD =
-                if config.secrets.grafana.enable then config.sops.placeholder."grafana/password" else "";
+                if (config.grafana.enable && config.secrets.grafana.enable) then
+                  config.sops.placeholder."grafana/password"
+                else
+                  "";
               NEXTCLOUD_USERNAME =
-                if config.secrets.nextcloud.enable then config.sops.placeholder."nextcloud/username" else "";
+                if (config.nextcloud.enable && config.secrets.nextcloud.enable) then
+                  config.sops.placeholder."nextcloud/username"
+                else
+                  "";
               NEXTCLOUD_PASSWORD =
-                if config.secrets.nextcloud.enable then config.sops.placeholder."nextcloud/password" else "";
-              FORGEJO_KEY = if config.secrets.forgejo.enable then config.sops.placeholder."forgejo/key" else "";
-              IMMICH_KEY = if config.secrets.immich.enable then config.sops.placeholder."immich/key" else "";
+                if (config.nextcloud.enable && config.secrets.nextcloud.enable) then
+                  config.sops.placeholder."nextcloud/password"
+                else
+                  "";
+              FORGEJO_KEY =
+                if (config.forgejo.enable && config.secrets.forgejo.enable) then
+                  config.sops.placeholder."forgejo/key"
+                else
+                  "";
+              IMMICH_KEY =
+                if (config.immich.enable && config.secrets.immich.enable) then
+                  config.sops.placeholder."immich/key"
+                else
+                  "";
               JELLYFIN_KEY =
-                if config.secrets.media-server.enable then config.sops.placeholder."jellyfin/key" else "";
+                if (config.media-server.enable && config.secrets.media-server.enable) then
+                  config.sops.placeholder."jellyfin/key"
+                else
+                  "";
               RADARR_KEY =
-                if config.secrets.media-server.enable then config.sops.placeholder."radarr/key" else "";
+                if (config.media-server.enable && config.secrets.media-server.enable) then
+                  config.sops.placeholder."radarr/key"
+                else
+                  "";
               SONARR_KEY =
-                if config.secrets.media-server.enable then config.sops.placeholder."sonarr/key" else "";
+                if (config.media-server.enable && config.secrets.media-server.enable) then
+                  config.sops.placeholder."sonarr/key"
+                else
+                  "";
               LIDARR_KEY =
-                if config.secrets.media-server.enable then config.sops.placeholder."lidarr/key" else "";
+                if (config.media-server.enable && config.secrets.media-server.enable) then
+                  config.sops.placeholder."lidarr/key"
+                else
+                  "";
               PROWLARR_KEY =
-                if config.secrets.media-server.enable then config.sops.placeholder."prowlarr/key" else "";
+                if (config.media-server.enable && config.secrets.media-server.enable) then
+                  config.sops.placeholder."prowlarr/key"
+                else
+                  "";
             };
           };
           path = "/var/lib/rancher/k3s/server/manifests/homepage-secret.json";
